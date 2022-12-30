3edcf3305f00ca77c4f1c355028dd09e8162e05d
Tanuja <tanujakhatun292@gmail.com>
2022-12-31 01:42:50 +0530

    navbar changed

--- src/Component/Navebar.js
+++ src/Component/Navebar.js
@@ -3,28 +3,30 @@ import React from 'react'
 export default function Navebar() {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+      <a className="navbar-brand" href="#">
+        <h3><i className="fa-solid fa-car"></i>Port<span class="text-danger">folio</span></h3>
+      </a>
+      <div className="collapse navbar-collapse " id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link text-white " href="#">HOME<span class="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="">ABOUT</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="">SKILLS</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white " href="ebook.html">SERVICES</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white " href="ebook.html">CONTACT</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
     </div>
   )
 }
