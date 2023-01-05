6ce963ad344b9795bb0d2247f7d140bc510bd68b
Tanuja <tanujakhatun292@gmail.com>
2023-01-05 23:56:32 +0530

    image added

--- src/Component/Skill.js
+++ src/Component/Skill.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import './Style.css'
 export default function Skill() {
   return (
     <div>
-      <div className='row'>
-        <div className='col-lg-4'>
+      <div className='row m-5'>
+        <div className='col-lg-4 skillimages'>
         <img src='https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_960_720.png' alt="" className='w-50 h-40  ' srcset="" />
         </div>
       </div>
