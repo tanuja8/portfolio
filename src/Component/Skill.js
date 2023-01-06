3035e1a51d933be2d4f65c018e9c1419ebbe7b5b
Tanuja <tanujakhatun292@gmail.com>
2023-01-06 23:29:30 +0530

    card added

--- src/Component/Skill.js
+++ src/Component/Skill.js
@@ -3,11 +3,13 @@ import './Style.css'
 export default function Skill() {
   return (
     <div>
-      <div className='row m-5'>
-        <div className='col-lg-4 skillimages'>
-        <img src='https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_960_720.png' alt="" className='w-50 h-40  ' srcset="" />
-        </div>
+     <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwHQmnvePItAA1-DcKX_S-5qgiXPW2h3Aos351vfu5vI8N6tvv0powsIksM4EeE4p77jc&usqp=CAU" alt="Card image cap"/>
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
       </div>
-    </div>
+    
   )
 }
