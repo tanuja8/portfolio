2a3597dd2a52e24554cf2b359139486bd59c655f
Tanuja <tanujakhatun292@gmail.com>
2023-01-08 00:39:28 +0530

    skill part designed

--- src/Component/Skill.js
+++ src/Component/Skill.js
@@ -6,14 +6,14 @@ export default function Skill() {
       <div className='row m-5 '>
         <div className='col-lg-2 '>
     <div class="card " >
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpdOA9a03E7X32tGj674zMXp1cjm0tA6BZsQ&usqp=CAU" class="card-img-top w-50 mx-auto my-3" alt="..."/>
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpdOA9a03E7X32tGj674zMXp1cjm0tA6BZsQ&usqp=CAU" class="card-img-top  mx-auto my-3" alt="..."/>
   <div class="card-body">
     <h5 class="card-title">HTML</h5>
     </div>
   </div>
 </div>
 <div className='col-lg-2'>
-    <div class="card" >
+    <div class="card " >
   <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png" class="card-img-top w-50 mx-auto my-3" alt="..."/>
   <div class="card-body">
     <h5 class="card-title">CSS</h5>
@@ -21,7 +21,7 @@ export default function Skill() {
   </div>
 </div>
 <div className='col-lg-2'>
-    <div class="card" >
+    <div class="card " >
   <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1280px-Bootstrap_logo.svg.png" class="card-img-top w-50 mx-auto my-3" alt="..."/>
   <div class="card-body">
     <h5 class="card-title">BOOTSTRAP</h5>
@@ -37,18 +37,18 @@ export default function Skill() {
   </div>
 </div>
 <div className='col-lg-2'>
-    <div class="card">
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpdOA9a03E7X32tGj674zMXp1cjm0tA6BZsQ&usqp=CAU" class="card-img-top w-50 mx-auto my-3" alt="..."/>
+    <div class="card ">
+  <img src="https://www.svgrepo.com/show/306646/react.svg" class="card-img-top w-50 mx-auto my-3" alt="..."/>
   <div class="card-body">
-    <h5 class="card-title">HTML</h5>
+    <h5 class="card-title">REACT</h5>
     </div>
   </div>
 </div>
-<div className='col-lg-2'>
-    <div class="card" >
-  <img src="https://www.svgrepo.com/show/306646/react.svg" class="card-img-top w-50 mx-auto my-3" alt="..."/>
+<div className='col-lg-2 '>
+    <div class="card " >
+  <img src="https://i.pinimg.com/originals/b1/5e/ed/b15eedbdafbbdbca3249e3942f4faf3b.png" class="card-img-top w-50 mx-auto my-3" alt="..."/>
   <div class="card-body">
-    <h5 class="card-title">HTML</h5>
+    <h5 class="card-title">GIT</h5>
     </div>
   </div>
 </div>
