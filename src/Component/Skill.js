7c1d5d94ddf912c41409773b9929d64fa5a50e6f
Tanuja <tanujakhatun292@gmail.com>
2023-01-08 00:01:51 +0530

    image changed

--- src/Component/Skill.js
+++ src/Component/Skill.js
@@ -3,50 +3,50 @@ import './Style.css'
 export default function Skill() {
   return (
     <div>
-      <div className='row m-5 text-center'>
+      <div className='row m-5 '>
         <div className='col-lg-2 '>
-    <div class="card" style={{width: "15rem"}}>
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpdOA9a03E7X32tGj674zMXp1cjm0tA6BZsQ&usqp=CAU" class="card-img-top w-50" alt="..."/>
+    <div class="card " >
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpdOA9a03E7X32tGj674zMXp1cjm0tA6BZsQ&usqp=CAU" class="card-img-top w-50 mx-auto my-3" alt="..."/>
   <div class="card-body">
     <h5 class="card-title">HTML</h5>
     </div>
   </div>
 </div>
 <div className='col-lg-2'>
-    <div class="card" style={{width: "18rem"}}>
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpdOA9a03E7X32tGj674zMXp1cjm0tA6BZsQ&usqp=CAU" class="card-img-top w-50" alt="..."/>
+    <div class="card" >
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png" class="card-img-top w-50 mx-auto my-3" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">CSS</h5>
+    </div>
+  </div>
+</div>
+<div className='col-lg-2'>
+    <div class="card" >
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/1280px-Bootstrap_logo.svg.png" class="card-img-top w-50 mx-auto my-3" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">BOOTSTRAP</h5>
+    </div>
+  </div>
+</div>
+<div className='col-lg-2'>
+    <div class="card" >
+  <img src="https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png" class="card-img-top w-50 mx-auto my-3" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">JAVASCRIPT</h5>
+    </div>
+  </div>
+</div>
+<div className='col-lg-2'>
+    <div class="card">
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpdOA9a03E7X32tGj674zMXp1cjm0tA6BZsQ&usqp=CAU" class="card-img-top w-50 mx-auto my-3" alt="..."/>
   <div class="card-body">
     <h5 class="card-title">HTML</h5>
     </div>
   </div>
 </div>
 <div className='col-lg-2'>
-    <div class="card" style={{width: "18rem"}}>
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpdOA9a03E7X32tGj674zMXp1cjm0tA6BZsQ&usqp=CAU" class="card-img-top w-50" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">HTML</h5>
-    </div>
-  </div>
-</div>
-<div className='col-lg-2'>
-    <div class="card" style={{width: "18rem"}}>
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpdOA9a03E7X32tGj674zMXp1cjm0tA6BZsQ&usqp=CAU" class="card-img-top w-50" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">HTML</h5>
-    </div>
-  </div>
-</div>
-<div className='col-lg-2'>
-    <div class="card" style={{width: "18rem"}}>
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpdOA9a03E7X32tGj674zMXp1cjm0tA6BZsQ&usqp=CAU" class="card-img-top w-50" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">HTML</h5>
-    </div>
-  </div>
-</div>
-<div className='col-lg-2'>
-    <div class="card" style={{width: "18rem"}}>
-  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpdOA9a03E7X32tGj674zMXp1cjm0tA6BZsQ&usqp=CAU" class="card-img-top w-50" alt="..."/>
+    <div class="card" >
+  <img src="https://www.svgrepo.com/show/306646/react.svg" class="card-img-top w-50 mx-auto my-3" alt="..."/>
   <div class="card-body">
     <h5 class="card-title">HTML</h5>
     </div>
