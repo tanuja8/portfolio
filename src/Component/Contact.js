b98bce6fd790be96b7e3e7b969dda76efe855a72
Tanuja <tanujakhatun292@gmail.com>
2023-01-12 11:25:22 +0530

    contact area designed

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -3,12 +3,12 @@ import './Style.css'
 export default function Contact() {
   return (
     <div>
-      <div className='container '>
+      <div className=' contactimg m-3 '>
         <h3 className='text-center  mt-5'>Contact</h3>
-        <span className='text-success'>Contact With Me</span>
+        <span className=' text-white'>Contact With Me</span>
         <div className='row m-5'>
-          <div className='col-lg-4 text-left'>
-            <h3 className=''>Let's chat<br />
+          <div className='col-lg-3 text-left'>
+            <h3 >Let's chat<br />
               Tell me about your Project
             </h3>
             <span>8927933406</span><br />
@@ -17,15 +17,14 @@ export default function Contact() {
             <div className='row'>
             </div>
           </div>
-          <div className='col-lg-8 '>
-            <form className='text-center'>
+          <div className='col-lg-9 '>
+            <form className='text-left'>
               <input type="text" placeholder='Enter Your Name' className='w-50 p-2' /> <br></br>
               <input type="email" placeholder='Enter your email' className='my-3 w-50 p-2' /> <br></br>
               <input type="text" placeholder='Enter your Phone Number' className='mb-3 w-50 p-2' /> <br></br>
               <textarea id="freeform" name="freeform" rows="4" cols="50" placeholder=' Type your Message...'>
-               
               </textarea><br />
-              <button type="button" className="btn btn-danger  mt-3">Send Message</button>
+              <button type="button" className="btn btn-danger  mt-3 ">Send Message</button>
             </form>
           </div>
         </div>
